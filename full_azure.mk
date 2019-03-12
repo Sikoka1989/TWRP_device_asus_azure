d76cac7072c4456c5e6bbdd7a523c1f4afebc9dd
Xsanori <elxsanori@gmail.com>
2019-03-12 19:50:59 +0300

    Add files via git

---
+++ full_azure.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+PRODUCT_DEVICE := azure
+PRODUCT_NAME := full_azure
+PRODUCT_BRAND := Asus
+PRODUCT_MODEL := Zenfone 2 Laser
+PRODUCT_MANUFACTURER := Asus
---
+++ omni_azure.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, vendor/omni/config/common.mk)
+
+$(call inherit-product, device/asus/azure/full_azure.mk)
+
+PRODUCT_NAME := omni_azure
